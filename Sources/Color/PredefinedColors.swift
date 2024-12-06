513336c08dedc913e3ceaa24c8bc3472fbc77000
Blueyds <9055449+blueyds@users.noreply.github.com>
2024-12-05 20:17:15 -0600

    add new colors and lessened the value on the green

--- Sources/Color/PredefinedColors.swift
+++ Sources/Color/PredefinedColors.swift
@@ -2,7 +2,7 @@ extension GameColor{
 	static public var black:   GameColor {GameColor( 0.0,  0.0,  0.0, 1.0 )}
 	static public var navy:    GameColor {GameColor( 0.0,  0.0,  0.5, 1.0 )}
 	static public var blue:    GameColor {GameColor( 0.0,  0.0,  1.0, 1.0 )}
-	static public var green:   GameColor {GameColor( 0.0,  0.5,  0.0, 1.0 )}
+	static public var green:   GameColor {GameColor( 0.2,  0.8,  0.2, 1.0 )}
 	static public var teal:    GameColor {GameColor( 0.0,  0.5,  0.5, 1.0 )}
 	static public var lime:    GameColor {GameColor( 0.0,  1.0,  0.0, 1.0 )}
 	static public var aqua:    GameColor {GameColor( 0.0,  1.0,  1.0, 1.0 )}
@@ -15,4 +15,5 @@ extension GameColor{
 	static public var fuchsia: GameColor {GameColor( 1.0,  0.0,  1.0, 1.0 )}
 	static public var yellow:  GameColor {GameColor( 1.0,  1.0,  0.0, 1.0 )}
 	static public var white:   GameColor {GameColor( 1.0,  1.0,  1.0, 1.0 )}
+	static public var blueSky: GameColor {GameColor( 0.3,  0.4,  0.8, 1.0 )}
 }
